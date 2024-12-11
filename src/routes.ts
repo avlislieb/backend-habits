336427ab450b2958270b1536eee5b212e47468fc
gabriel silva <gabriel.silva@toolzz.me>
2024-12-10 23:27:52 -0300

    include prisma

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
+import { PrismaClient } from "@prisma/client";
 import { FastifyInstance } from "fastify";
 
+const prisma = new PrismaClient();
+
 export default async function appRoute(app: FastifyInstance) {
-  app.get('/habits', () => {
-    return 'hello'
+  app.get('/habits', async (req, res) => {
+    const habits = await prisma.habit.findMany()
+    return res.send({habits})
   })
   
 }
